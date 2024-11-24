0127acf7b381bdc7dea107e57fb8b0a27e634dac
dijith481 <dijithdinesh481@gmail.com>
2024-11-24 11:48:14 +0530

    cycle3

---
+++ S1/python/lab/cycle3/6.py
@@ -0,0 +1,10 @@
+from math import factorial
+
+n = 5
+for i in range(n):
+    print("n=", i, end="")
+    for j in range(n - i + 1):
+        print(end=" ")
+    for j in range(i + 1):
+        print(factorial(i) // (factorial(j) * factorial(i - j)), end=" ")
+    print()
