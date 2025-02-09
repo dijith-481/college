14e15a4f2b53538338e436150fa905a621cef832
dijith-481 <dijithdinesh@protonmail.com>
2025-02-09 14:14:22 +0530

    lab1

--- s2/c/1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-  int a, b, c;
-  scanf("%d", &a);
-  printf("%d%c%d", a, a, b);
-}
---
+++ s2/c/lab/lab1/star-pattern.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  printf("*\n");
+  printf("*\t*\n");
+  printf("*\t*\t*\n");
+  return 0;
+}
